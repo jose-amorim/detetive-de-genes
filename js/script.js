1402401546f34994912fa7aa1d7132b7d2410262
José Amorim <josejamorim21@gmail.com>
2023-04-18 00:34:27 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -98,7 +98,7 @@ $(document).ready(() => {
     const lista_rodadas = [
         new Rodada(
             1,
-            '/img/heredograma1.png', // './img/heredograma1.png',
+            './img/heredograma1.png', // './img/heredograma1.png',
             'Este é o seu primeiro caso! No heredograma acima, os indivíduos azuis são portadores de uma certa característica... e isso é tudo que sabemos.',
             'Qual é o padrão de herança desta característica?',
             ['Autossômica recessiva',
@@ -113,7 +113,7 @@ $(document).ready(() => {
             ]),
         new Rodada(
             2,
-            '/img/heredograma2.png', // './img/heredograma2.png',
+            './img/heredograma2.png', // './img/heredograma2.png',
             "Agora, a dificuldade vai aumentar. Este é um heredograma de uma família com casos de albinismos (simbolos roxos), um distúrbio genético caracterizado pela ausência total ou parcial de melanina. Veja uma coisa nova: a seta indica quem é o probando, ou seja, o indivíduo que está sendo estudado.",
             "Qual é a chance dos indivíduos II.1 e II.2 terem um filho com albinismo?",
             ['1/2',
@@ -128,7 +128,7 @@ $(document).ready(() => {
             ]),
         new Rodada(
             3,
-            '/img/heredograma3.png', // './img/heredograma3.png',
+            './img/heredograma3.png', // './img/heredograma3.png',
             "Você sabia a surdez pode ter diversas causas genéticas diferentes? Ela pode ser causada por mutações em genes autossômicos, genes localizados no cromossomo X ou ainda por genes mitocondriais. Os heredogramas acima representam quatro famílias em que ocorre surdez nos indivíduos pintados.",
             "Qual família rejeita a possibilidade que a surdez tenha herança mitocondrial?",
             ['Família 1',
@@ -143,7 +143,7 @@ $(document).ready(() => {
             ]),
         new Rodada(
             4,
-            '/img/heredograma4.png', // './img/heredograma4.png',
+            './img/heredograma4.png', // './img/heredograma4.png',
             "Você está quase no final! O heredograma acima mostra o surgimento de atrofia muscular espinhal (AME) em um menino filho de um casal de primos (note como o casamento consanguíneo é representado por duas linhas ao invés de uma). Esta doença é autossômica recessiva e rara, causada por uma mutação em um alelo no gene SMN1, sendo que 1 a cada 50 pessoas não portadoras possuem o alelo mutante. Representamos uma pessoa que não possui a doença mas é portadora do alelo mutante com um pontinho colorido.",
             'A chance do casal II.1 e II.2 ter uma criança com AME é de 1 em:',
             ['50',
@@ -158,7 +158,7 @@ $(document).ready(() => {
             ]),
         new Rodada(
             5,
-            '/img/heredograma5.png', //' ./img/heredograma5.png',
+            './img/heredograma5.png', //' ./img/heredograma5.png',
             "Você chegou no desafio final! Neste heredograma, o indivíduo 1 é afetado pela síndrome de Marfan. O indivíduo 2 é casado com o indivíduo 1 e não é afetado pela síndrome de Marfan, estes tiveram 2 filhos, o indivíduo 4  é afetado e herda a doença, sua irmã não possui a doença. O indivíduo 4 casa-se com uma mulher que não possui traços da síndrome em sua família, estes têm 4 filhos, onde dois desses tem a síndrome, indivíduo 7 e 8, os outros não possuem manifestações da doença. A mulher indivíduo 7 casa-se com um homem sem traços da síndrome, tem três filhos, dois homens e uma mulher, onde um dos homens possui a síndrome, a mulher não possui e o segundo filho não se sabe ainda.",
             'Qual a probabilidade do  indivíduo 12 ter a síndrome de Marfan?',
             ['0%',
